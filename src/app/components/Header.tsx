8afc34cffc44a5e8a71867203987713023012742
Neil Rowland <neilrowland56@gmail.com>
2023-06-08 09:49:18 +0100

    Added topic and individual topic - with prop details

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+      <h2>Notion Minimal</h2>
+    </div>
+  );
+}
---
+++ src/app/topic/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function TopicPage() {
+  return (
+    <main className="flex flex-col">
+      <h2>Topic</h2>
+    </main>
+  );
+}
